8b2c8cf86020823a708d579685d5b57ed08a7e4d
Milla Loimuvirta <milla.loimuvirta@gmail.com>
2021-03-13 22:07:18 +0200

    test

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
     return (
       <div className="App">
       <div className="header">
-              <h1>THE NAME DATA</h1>
+              <h1>THE NAME DATA IN HERE</h1>
             
             </div>
             
